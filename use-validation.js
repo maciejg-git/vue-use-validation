d4e1610c19aa5a7ace70c7d625352e3ad336ba4f
maciejg-git <ender@localhost>
2023-02-21 14:27:44 +0100

    init commit

---
+++ use-validation.js
@@ -0,0 +1,188 @@
+import { ref, watch, unref } from "vue";
+import { globalValidators } from "./validators";
+
+let isFunction = (v) => typeof v === "function";
+
+let defaultStatus = {
+  touched: false,
+  dirty: false,
+  valid: false,
+  optional: false,
+  validated: false,
+};
+
+export default function useValidation(
+  rules,
+  localModel,
+  externalState,
+  onUpdateState,
+  onReset,
+  opts
+) {
+  let status = ref({ ...defaultStatus });
+
+  let state = ref("");
+
+  let messages = ref({});
+
+  let options = {
+    validateOn: opts.validateOn || "blur",
+    validateMode: opts.validateMode || "silent",
+  }
+
+  if (!isFunction(onUpdateState)) {
+    onUpdateState = () => {}
+  }
+
+  onUpdateState(status, state, messages);
+
+  watch(
+    localModel,
+    (value) => {
+      onValueUpdated(value)
+    },
+    { deep: true }
+  );
+
+  let validate = (value) => {
+    let newStatus = {
+      touched: status.value.touched,
+      validated: status.value.validated,
+      dirty: status.value.dirty || !!(value && !!value.length),
+    };
+
+    let newMessages = {};
+    let validationRules = Object.entries(rules);
+
+    newStatus.valid = validationRules.reduce((valid, [key, v]) => {
+      let validator =
+        globalValidators[key] || (isFunction(rules[key]) && rules[key]);
+
+      if (!validator) return valid;
+
+      let res = validator(value, v);
+
+      if (res === true) {
+        newStatus[key] = true;
+      } else {
+        newStatus[key] = false;
+        newMessages[key] = res;
+      }
+
+      return valid && newStatus[key];
+    }, true);
+
+    newStatus.optional = !rules.required && (value === "" || value === false);
+
+    status.value = newStatus;
+    messages.value = newMessages;
+  };
+
+  let onValueUpdated = (value) => {
+    validate(value);
+
+    state.value = updateState();
+
+    onUpdateState(status, state, messages);
+  };
+
+  let touch = () => {
+    validate(unref(localModel));
+
+    status.value.touched = true;
+
+    state.value = updateState();
+
+    onUpdateState(status, state, messages);
+  };
+
+  let formValidate = () => {
+    validate(unref(localModel));
+
+    status.value.validated = true;
+
+    state.value = updateState();
+
+    onUpdateState(status, state, messages);
+  };
+
+  let updateState = () => {
+    // external state is set, return it
+    if (externalState && externalState.value !== null) {
+      return externalState.value;
+    }
+
+    // optional input (not required and empty) cannot be valid or invalid, 
+    // return defalut state
+    if (status.value.optional) {
+      return "";
+    }
+
+    // input has not been yet interacted in any way, return current state
+    if (
+      !status.value.dirty &&
+      !status.value.touched &&
+      !status.value.validated
+    ) {
+      return state.value;
+    }
+
+    // input is validated manually, return current state
+    if (options.validateOn === "form" && !status.value.validated) {
+      return state.value;
+    }
+
+    // input is validated on blur, return current state
+    if (
+      options.validateOn === "blur" &&
+      !status.value.touched &&
+      !status.value.validated
+    ) {
+      return state.value;
+    }
+
+    // input is validated immediately, has been touched or validated manually
+    // and can change state
+    // for invalid inputs always return invalid state
+    if (!status.value.valid) {
+      return "invalid";
+    }
+
+    // for valid inputs return valid only in eager mode or when changing 
+    // from non default state
+    if (options.validateMode === "eager" || state.value !== "") {
+      return "valid";
+    }
+
+    // return default state
+    return state.value;
+  };
+
+  watch(
+    externalState,
+    () => {
+      state.value = updateState();
+    },
+    { immediate: true }
+  );
+
+  // reset
+
+  let reset = () => {
+    status.value = { ...defaultStatus };
+    state.value = "";
+    messages.value = {};
+    isFunction(onReset) && onReset()
+    onUpdateState(status, state, messages)
+  };
+
+  return {
+    status,
+    state,
+    messages,
+    onValueUpdated,
+    touch,
+    formValidate,
+    reset,
+  };
+}
---
+++ validators.js
@@ -0,0 +1,77 @@
+let validationMessages = {
+  required: "This field is required",
+  alpha: "Please enter only letters",
+  numeric: "Please enter only numbers",
+  alphanumeric: "Please enter only letters or numbers",
+  minLength: "Please enter at least %d characters",
+  maxLength: "Please enter up to %d characters",
+  minElements: "Please select at least %d elements",
+  maxElements: "Please select at least %d elements",
+  minValue: "",
+  maxValue: "",
+  email: "Please enter valid email address",
+  atLeastOneUppercase: "Please enter at least one uppercase character",
+  atLeastOneLowercase: "Please enter at least one lowercase character",
+  atLeastOneDigit: "Please enter at least one numeric character",
+  atLeastOneSpecial: "Please enter at least one special character",
+  sameAs: ""
+};
+
+export let setValidationMessage = (validator, message) => {
+  validationMessages[validator] = message
+}
+
+export let globalValidators = {
+  required: (value) => {
+    if (typeof value === "boolean") return value
+    if (Array.isArray(value)) return !!value.length || validationMessages.required;
+    return !!value && !!value.trim() || validationMessages.required;
+  },
+  minLength: (value, length) => {
+    return value.length >= length || validationMessages.minLength.replace('%d', length);
+  },
+  maxLength: (value, length) => {
+    return value.length <= length || validationMessages.maxLength.replace('%d', length);
+  },
+  minElements: (value, length) => {
+    return value.length >= length || validationMessages.minLength.replace('%d', length);
+  },
+  maxElements: (value, length) => {
+    return value.length <= length || validationMessages.maxLength.replace('%d', length);
+  },
+  alpha: (value) => {
+    return /^[a-zA-Z]+$/.test(value) || validationMessages.alpha;
+  },
+  numeric: (value) => {
+    return /^[0-9]+$/.test(value) || validationMessages.numeric;
+  },
+  alphanumeric: (value) => {
+    return /^[a-zA-Z0-9]+$/.test(value) || validationMessages.alphanumeric;
+  },
+  maxValue: (value, max) => {
+    return value <= max || validationMessages.maxValue.replace('%d', length);
+  },
+  minValue: (value, min) => {
+    return value >= min || validationMessages.minValue.replace('%d', length);
+  },
+  email: (value) => {
+    return /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.test(
+      value
+    ) || validationMessages.email;
+  },
+  atLeastOneUppercase: (value) => {
+    return /[A-Z]/.test(value) || validationMessages.atLeastOneUppercase;
+  },
+  atLeastOneLowercase: (value) => {
+    return /[a-z]/.test(value) || validationMessages.atLeastOneLowercase;
+  },
+  atLeastOneSpecial: (value) => {
+    return /[^a-zA-Z0-9]/.test(value) || validationMessages.atLeastOneSpecial;
+  },
+  atLeastOneDigit: (value) => {
+    return /[0-9]/.test(value) || validationMessages.atLeastOneDigit;
+  },
+  sameAs: (value, value2) => {
+    return value === value2 || validationMessages.sameAs;
+  }
+};
